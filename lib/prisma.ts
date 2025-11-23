16f4f7047705a4b5b9d7f914a16b83f231a08716
ALGenesis <amedeelg02@gmail.com>
2025-11-23 18:23:56 +0100

    fix : prisma setup

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,6 @@
-import "dotenv/config";
 import { PrismaClient } from '../generated/prisma/client'
 
 const prisma = new PrismaClient()
+// use `prisma` in your application to read and write data in your DB
 
-export { prisma }
+export {prisma}
