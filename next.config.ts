c921ad6f5a7fb0c1021749abdbd24922d1a1113c
ALGenesis <amedeelg02@gmail.com>
2025-11-24 19:30:29 +0100

    fix : engine Type

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  outputFileTracingIncludes: {
+    '/api/*': ['./node_modules/.prisma/client/*.wasm']
+  },
 };
 
 export default nextConfig;
